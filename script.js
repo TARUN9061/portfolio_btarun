0366f47fed2dd3eba2d9f07eefaa5d1160c9a0c2
Tarun <b.tarun9902717714@gmail.com>
2023-07-21 17:40:58 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+var github = document.getElementById("github");
+github.href="https://github.com/tarun9061";
+
+var linkedin = document.getElementById("linkedin");
+linkedin.href="https://www.linkedin.com/in/b-tarun-5205b4245/";
